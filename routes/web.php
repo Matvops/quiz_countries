33146803717d0a35d2472e0cd0dd8447794ccedd
Matheus <matheusmartinscia@gmail.com>
2025-03-28 18:32:20 -0300

    app_data added

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,10 @@
 <?php
 
+use App\Http\Controllers\MainController;
+use Illuminate\Support\Facades\Route;
 
+Route::get('/', function() {
+    echo "Hello World!";
+});
+
+Route::get('/filled', [MainController::class, 'isFilled'])->name('filled');
