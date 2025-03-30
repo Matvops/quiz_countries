ba8f1ea107801b060bccd9d28b1330799792c5e3
Matheus <matheusmartinscia@gmail.com>
2025-03-30 18:15:41 -0300

    Criação da página home, método startGame e prepareGame

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
@@ -13,7 +14,33 @@ class MainController extends Controller
         $this->app_data = require(app_path('app_data.php'));
     }
 
-    public function isFilled(){
-        return response()->json($this->app_data);
-    }
+   public function startGame(): View
+   {
+        return view('home');
+   }
+
+   public function prepareGame(Request $request){
+        $request->validate(
+            [
+                'total_questions' => 'required|integer|min:3|max:30'
+            ],
+            [
+                'total_questions.required' => 'NÚMERO DE QUESTÕES É OBRIGATÓRIO',
+                'total_questions.integer' => 'DEVE SER UM NÚMERO INTEIRO',
+                'total_questions.min' => 'NÚMERO DE QUESTÕES MÍNIMO É :min',
+                'total_questions.max' => 'NÚMERO DE QUESTÕES MÁXIMO É :max'
+            ]
+        );
+
+        $total_questions = intval($request->input('total_questions'));
+
+        $quiz = $this->prepareQuiz($total_questions);
+
+        dd($quiz);
+   }
+
+   private function prepareQuiz($questions){
+
+        return null;
+   }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,5 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home');
-
-Route::get('/filled', [MainController::class, 'isFilled'])->name('filled');
+Route::get('/', [MainController::class, 'startGame'])->name('start');
+Route::post('/', [MainController::class, 'prepareGame'])->name('prepare');
