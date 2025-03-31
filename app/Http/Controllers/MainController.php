507cfdc628b619fb8a74d9cc7e68c409a0cb8572
Matheus <matheusmartinscia@gmail.com>
2025-03-30 21:14:56 -0300

    Logic of the game has been created

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -39,8 +39,32 @@ class MainController extends Controller
         dd($quiz);
    }
 
-   private function prepareQuiz($questions){
+   private function prepareQuiz($total_questions){
 
-        return null;
+        $questions = [];
+        $total_countries = count($this->app_data) - 1;
+
+        $indexes = range(0, $total_countries);
+        shuffle($indexes);
+        $indexes = array_slice($indexes, 0, $total_questions);
+
+        $question_number = 1;
+        foreach($indexes as $index){
+            $question['question_number'] = $question_number++;
+            $question['country'] = $this->app_data[$index]['country'];
+            $question['correct_answer'] = $this->app_data[$index]['capital'];
+
+            $other_capitals = array_column($this->app_data, 'capital');
+
+            $other_capitals = array_diff($other_capitals, [$question['correct_answer']]); 
+
+            shuffle($other_capitals);
+            $question['wrong_answer'] = array_slice($other_capitals, 0, 3);
+
+            $question['correct'] = null;
+            $questions[] = $question;
+        }
+
+       return $questions;
    }
 }
